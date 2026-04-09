f111845bcc3a0dce22eafaf0c5a42a30639e2dc3
Esteban Perez <esteban.perez@wsu.edu>
2026-04-09 04:28:37 +0000

    added Header scroll

--- script.js
+++ script.js
@@ -7,6 +7,45 @@ scrollToTopBtn.addEventListener("click", function() {
     });
 });
 
+let header = document.getElementById("mainHeader");
+let headerTextHome = document.querySelector(".home");
+let headerTextAbout = document.querySelector(".about");
+let headerTextProjects = document.querySelector(".projects");
+let headerTextContact = document.querySelector(".contact");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
+    headerTextHome.style.display = "none";
+    headerTextAbout.style.display = "none";
+    headerTextProjects.style.display = "none";
+    headerTextContact.style.display = "none";
+  } else {
+    header.classList.remove("shrink");
+    headerTextHome.style.display = "block";
+    headerTextAbout.style.display = "block";
+    headerTextProjects.style.display = "block";
+    headerTextContact.style.display = "block";
+  }
+});
+
+window.addEventListener("mouseenter", () => {
+    header.classList.add("shrink");
+    headerTextHome.style.display = "block";
+    headerTextAbout.style.display = "block";
+    headerTextProjects.style.display = "block";
+    headerTextContact.style.display = "block";
+    });
+
+window.addEventListener("mouseleave", () => {
+    header.classList.remove("shrink");
+    headerTextHome.style.display = "block";
+    });
+
+
+
+
+
     //run this code when the DOM loads
 document.addEventListener('DOMContentLoaded', function (e) {
 
